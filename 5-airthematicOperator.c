74a991b92a4b863c13c9b3d9b9f6a2db85ada84a
Sanjeev v. Gaikwad <113491985+sanjeevvgaikwad37@users.noreply.github.com>
2023-08-27 16:21:53 -0500

    Create 5-airthematicOperator.c

---
+++ 5-airthematicOperator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+  int a;
+  float b;
+  a = 34;
+  b = 6;
+  printf("a + b = %f\n",a+b);
+  printf("a - b = %f\n",a-b);
+  printf("a * b = %f\n",a*b);
+  printf("a / b = %f\n",a/b);
+    return 0;
+}
