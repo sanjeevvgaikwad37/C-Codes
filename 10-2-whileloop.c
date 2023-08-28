a175fcee8d5856044a77d847947b766313f05620
Sanjeev v. Gaikwad <113491985+sanjeevvgaikwad37@users.noreply.github.com>
2023-08-28 15:31:20 -0500

    Create 10-2-whileloop.c
    simple example to demonstrate multiplication table of 3 using while loop

---
+++ 10-2-whileloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    printf("Printing Multiplication Table of 3");
+    while (i<=30)
+    {
+       printf("%d\n", i);
+        i +=3;
+    }
+    
+}
