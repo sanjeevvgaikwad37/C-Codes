47c7547a547a360d511f9eb37c3e8ac3c6bfa297
Sanjeev v. Gaikwad <113491985+sanjeevvgaikwad37@users.noreply.github.com>
2023-09-02 14:56:51 -0500

    Create 10-6-printNumberbyForloop.c
    printing numbers 1 to 50 by foor loop

---
+++ 10-6-printNumberbyForloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    int i, j=0;
+    for(i = 0; i < 50;  i++, j++)
+    {
+      printf("%d %d\n",i, j);
+      
+    }
+    
+    return 0;
+}
